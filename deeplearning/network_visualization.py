visualization fix: regularization after gradient

deeplearning/network_visualization.py
@@ -116,10 +116,11 @@ def update_class_visulization(model, target_y, l2_reg, learning_rate, img):
     # L2 regularization term!                                              #
     # Be very careful about the signs of elements in your code.            #
     ########################################################################
-    scores = model(img)
-    scores[:, target_y].backward()
-    dimg = img.grad.data - 2 * l2_reg * img.grad.data
-    img.data += learning_rate * (dimg/torch.norm(dimg))
+    scores = model(img)[:, target_y]
+    scores.backward()
+    
+    grad = img.grad.data - l2_reg * img.grad.data
+    img.data += learning_rate * grad/torch.norm(grad)
     img.grad.data = torch.zeros_like(img.grad.data)
     ########################################################################
     #                             END OF YOUR CODE                         #