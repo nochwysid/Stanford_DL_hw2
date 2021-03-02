visualization fix: reset gradient

deeplearning/network_visualization.py
@@ -117,9 +117,9 @@ def update_class_visulization(model, target_y, l2_reg, learning_rate, img):
     # Be very careful about the signs of elements in your code.            #
     ########################################################################
     scores = model(img)
-    (scores[:, target_y] - l2_reg * (torch.norm(img.data)**2)).backward()
-    dimg = learning_rate * img.grad.data / torch.norm(img.grad.data)
-    img.data += dimg.data
+    scores[:, target_y].backward()
+    dimg = img.grad.data - 2 * l2_reg * img.grad.data
+    img.data += learning_rate * (dimg/torch.norm(dimg))
     img.grad.data = torch.zeros_like(img.grad.data)
     ########################################################################
     #                             END OF YOUR CODE                         #